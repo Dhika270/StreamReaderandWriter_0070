476f8302c3f3d2c256c91f411659add5c94ef9c6
Dhika270 <andikasetiawan05ds@gmail.com>
2025-06-10 15:01:29 +0700

    return 0

--- iofile.cpp
+++ iofile.cpp
@@ -39,5 +39,10 @@ int main() {
             //dan tampilan di sini
             cout << baris << '\n';
         }
+        //tutup file tersebut setelah selesai
+        infile.close();
     }
-};
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "unable to open file";
+    return 0;
+}
