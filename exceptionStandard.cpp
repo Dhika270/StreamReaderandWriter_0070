959d84f418f33906f5e7336217860e82faf0a0fd
Dhika270 <andikasetiawan05ds@gmail.com>
2025-06-10 15:07:34 +0700

    progres 11

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -4,3 +4,13 @@
 #include<array>
 //untuk array yang akan kita gunakan
 using namespace std;
+
+int main()
+{
+    cout << "Awal program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = { 10, 20, 30 };
+        //pesan array integer 3 elemen
+        cout<< data.at(5)<<endl;
+    }
+}
