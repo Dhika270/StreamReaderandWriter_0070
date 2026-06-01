7fb6c279dc7114694bc7138b433a5a4420f75e75
Dhika270 <andikasetiawan05ds@gmail.com>
2026-06-01 18:14:24 +0700

    menunjuk ke sebuah file

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -34,6 +34,13 @@ int main()
 
     //membuka file dalam mode membaca
     ifstream infile;
-     
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">=Membuka dan membaca file " << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
+    {
+        
         
 }
